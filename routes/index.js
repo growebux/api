8eb02f0923620fa28775f6ee7a6cfe127878049e
Rafael <growebux@gmail.com>
2021-10-07 12:37:38 -0500

    File renamed

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const { Router } = require("express");
 const { productsRouter } = require("./products");
+const { usersRouter } = require("./users");
 const { userAuthenticatedMiddleware } = require("../middlewares");
 
 const routes = Router();
 
 routes.use("/products", userAuthenticatedMiddleware, productsRouter);
-
+app.use("/users", userAuthenticatedMiddleware, usersRouter);
 module.exports = {
   routes,
 };
+
+
+
